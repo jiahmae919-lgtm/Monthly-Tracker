f78704968169d1530c1b16900625aeea7b7fbf4d
Christian Delapos <christian.delapos6@gmail.com>
2026-04-22 14:44:46 +0800

    updated at: 04/22/26

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Import the URL facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS if the application is running in the production environment
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
